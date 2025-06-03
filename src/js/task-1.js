c91ee42014c129289ceb89be9f15f5643d8cf1f7
Veronika <mnika5437@gmail.com>
2025-06-03 20:35:41 +0300

    add css style to task1 and done task1js

---
+++ src/js/task-1.js
@@ -0,0 +1,10 @@
+const categoriesList = document.querySelectorAll("#categories .item");
+console.log(`Number of categories: ${categoriesList.length}`);
+
+categoriesList.forEach((category) => {
+  const categoryTitle = category.querySelector("h2").textContent;
+  const itemsCount = category.querySelectorAll("ul li").length;
+
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Elements: ${itemsCount}`);
+});
