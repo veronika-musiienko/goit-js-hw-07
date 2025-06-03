4077b6b8725b0263a4bc598ee5f1c144fd1d9f26
Veronika <mnika5437@gmail.com>
2025-06-03 21:05:05 +0300

    add css style gallery and done task2

--- src/js/task-2.js
+++ src/js/task-2.js
@@ -24,3 +24,12 @@ const images = [
     alt: "Lighthouse Coast Sea",
   }
 ];
+
+const galleryElements = document.querySelector(".gallery");
+const galleryItems = images
+  .map(
+    ({ url, alt }) =>
+      `<li class="gallery-item"><img src="${url}" alt="${alt}" class="gallery-image"></li>`
+  )
+  .join("");
+galleryElements.insertAdjacentHTML("beforeend", galleryItems);
